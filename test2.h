eb8e41d3b5bd0eff5615419cb0696cdf7981dc0b
MassGH2023 <122106065+MassGH2023@users.noreply.github.com>
2025-02-02 13:58:48 -0500

    Feb2

---
+++ test2.h
@@ -0,0 +1,12 @@
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main() {
+
+    int num1 = 5;
+    int num2 = 10;
+    std::cout << "The sum is: " << add(num1, num2) << std::endl;
+    return 0;
+}
